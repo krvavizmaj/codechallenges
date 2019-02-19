0b3d2e53c8c0198ce06d315d8083dca94fe2756c
ROOT\ArsovV <vladimir.arsov@baywa.de>
2019-02-19 16:44:53 +0100

    hh

--- src/com/codechef/TREEROOT.py
+++ src/com/codechef/TREEROOT.py
@@ -1,35 +1,35 @@
-def findRoot(n, k, currentNode, childSum, childCount, parent):
+def findRoot(n, k, currentNodeIndex, nodesMap, childSum, childCount, parent):
     if k == n:
         return parent
     for i in range(1, n+1):
-        if i != currentNode and childSum[i] >= currentNode and childCount[i] < 2 and parent[i] != currentNode:
-            childSum[i] -= currentNode
+        if i != currentNodeIndex and childSum[i] >= nodesMap[currentNodeIndex] and childCount[i] < 2 and parent[i] != currentNodeIndex:
+            childSum[i] -= nodesMap[currentNodeIndex]
             childCount[i] += 1
-            parent[currentNode] = i
+            parent[currentNodeIndex] = i
             
             for j in range(1, n+1):
                 if parent[j] == 0:
-                    return findRoot(n, k + 1, j, childSum, childCount, parent)
+                    return findRoot(n, k + 1, j, nodesMap, childSum, childCount, parent)
 
-            parent[currentNode] = 0
+            parent[currentNodeIndex] = 0
             childCount[i] -= 1
-            childSum[i] += currentNode
+            childSum[i] += nodesMap[currentNodeIndex]
 
 testCases = input()
 for t in range(0, int(testCases)):
     n = int(input())
-    nodeMap = [0 for _ in range(0, n+1)]
+    nodesMap = [0 for _ in range(0, n+1)]
     childSum = [0 for _ in range(0, n+1)]
     childCount = [0 for _ in range(0, n+1)]
     parent = [0 for _ in range(0, n+1)]
 
     for k in range(0, n):
         line = input()
-        nodeMap[k+1] = int(line.split(" ")[0])
+        nodesMap[k+1] = int(line.split(" ")[0])
         childSum[k+1] = int(line.split(" ")[1])
-    tree = findRoot(n, 1, 1, childSum, childCount, parent)
+    tree = findRoot(n, 1, 1, nodesMap, childSum, childCount, parent)
 
     k = 1
     while tree[k] != 0:
         k += 1
-    print(nodeMap[k])
+    print(nodesMap[k])
