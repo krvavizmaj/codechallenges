caed819363ca1e95cf89b66f8c1b9dbbf3f7ee8c
Vladimir Arsov <krvavizmaj@gmail.com>
2019-12-05 13:09:51 +0100

    Final solution 1 and solution 2

--- src/phillipscodechalenge/Solution.java
+++ src/phillipscodechalenge/Solution.java
@@ -1,14 +1,9 @@
-/*
- * Copyright 2019, collaboration Factory AG. All rights reserved.
- */
-
 import java.io.*;
 import java.math.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Result {
-    static double pi = 3.14159265359;
+    static BigDecimal pi = new BigDecimal("3.14159265359");
     static double[][] result;
     static List<Integer> r;
     static int executions = 0;
@@ -33,10 +28,10 @@ class Result {
     public static void largestSegmentBF(List<Integer> radius, List<Double> max, int[] dist, int index, int segments) {
         if (index >= dist.length) {
             if (segments == 0) {
-                double m = (double)radius.get(0) * (double)radius.get(0) * pi;
+                double m = (double)radius.get(0) * (double)radius.get(0) * pi.doubleValue();
                 for (int i = 0; i < dist.length; i++) {
                     if (dist[i] > 0) {
-                        m = Math.min(m, (double)radius.get(i) * (double)radius.get(i) * pi / (double)dist[i]);
+                        m = Math.min(m, (double)radius.get(i) * (double)radius.get(i) * pi.doubleValue() / (double)dist[i]);
                     }
                 }
                 max.set(0, Math.max(m, max.get(0)));
@@ -57,10 +52,10 @@ class Result {
 
         result = new double[n][segments];
         for (int i = 0; i < segments; i++) {
-            result[0][i] = pi * radius.get(0) * radius.get(0) / (double)(i+1);
+            result[0][i] = pi.doubleValue() * radius.get(0) * radius.get(0) / (double)(i+1);
         }
         for (int i = 0; i < n; i++) {
-            result[i][0] = pi * radius.get(i) * radius.get(i);
+            result[i][0] = pi.doubleValue() * radius.get(i) * radius.get(i);
         }
 
         for (int j = 1; j < segments; j++) {
@@ -104,9 +99,9 @@ class Result {
         if (result[row][col] == 0) {
             executions++;
             if (row == 0 || col == 0) {
-                result[row][col] = pi * r.get(row) * r.get(row) / (double)(col+1);
+                result[row][col] = pi.doubleValue() * r.get(row) * r.get(row) / (double)(col+1);
             } else {
-                double area = pi * r.get(row) * r.get(row);
+                double area = pi.doubleValue() * r.get(row) * r.get(row);
                 result[row][col] = area / (double)(col+1);
 
                 int intervalStart = 0;
@@ -141,28 +136,55 @@ class Result {
         }
     }
 
-// =====================================================================================================================
+// Solution 4 (Straight forward) =============================================================================================
+    static BigDecimal[] area;
+    static boolean[] checked;
     public static String largestSegmentStraightforward(List<Integer> radius, int segments) {
         int n = radius.size();
         Collections.sort(radius);
         Collections.reverse(radius);
-        List<Double> area = radius.stream().map(x -> x * x * pi).collect(Collectors.toList());
 
-        int[] dist = new int[radius.size()];
-        dist[0] = segments;
+        area = new BigDecimal[n];
+        for (int i = 0; i < n; i++) area[i] = BigDecimal.valueOf(radius.get(i) * radius.get(i)).multiply(pi);
+        checked = new boolean[segments+1];
 
-        double max = area.get(0) / segments;
-        boolean running = true;
-        int current = 0;
-        while (running) {
-            while (area.get(current) / dist[0] )
-
-            running = false;
+        int intervalStart = 1;
+        int intervalEnd = segments;
+        while (intervalEnd - intervalStart > 1) {
+            int middle = (intervalStart + intervalEnd) / 2;
+            checked[middle] = true;
+            BigDecimal dividedArea = area[0].divide(BigDecimal.valueOf(middle), RoundingMode.HALF_UP);
+            int largerSegments = 0;
+            int i = 1;
+            while (i < n && area[i].divide(dividedArea, RoundingMode.HALF_UP).intValue() > 0 && largerSegments < segments - middle) {
+                largerSegments += area[i].divide(dividedArea, RoundingMode.HALF_UP).intValue();
+                i++;
+            }
+            if (largerSegments >= segments - middle) {
+                intervalEnd = middle;
+            } else {
+                intervalStart = middle;
+            }
         }
 
-        BigDecimal r = new BigDecimal(Double.toString(0));
-        r = r.setScale(4, RoundingMode.HALF_EVEN);
-        String s = r.toString();
+        BigDecimal max = area[0].divide(BigDecimal.valueOf(intervalEnd), RoundingMode.HALF_UP);
+        BigDecimal previousValue = area[0].divide(BigDecimal.valueOf(intervalStart), RoundingMode.HALF_UP);
+        List<BigDecimal> maxCandidates = new ArrayList<>();
+        int i = 1;
+        while (i < n && area[i].divide(max, RoundingMode.HALF_UP).intValue() > 0) {
+            for (int j = 1; j <= area[i].divide(max, RoundingMode.HALF_UP).intValue(); j++) {
+                maxCandidates.add(area[i].divide(BigDecimal.valueOf(j), RoundingMode.HALF_UP));
+            }
+            i++;
+        }
+        Collections.sort(maxCandidates);
+        Collections.reverse(maxCandidates);
+        if (maxCandidates.size() >= segments - intervalEnd + 1 && maxCandidates.get(segments - intervalEnd).compareTo(max) > 0) {
+            max = maxCandidates.get(segments - intervalEnd);
+        }
+
+        max = max.setScale(4, RoundingMode.HALF_UP);
+        String s = max.toString();
         return s;
     }
 
@@ -171,7 +193,11 @@ class Result {
 public class Solution {
     public static void main(String[] args) throws IOException {
 
-//        Integer[] radius = new Integer[] {2, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9, 9, 10, 12, 14, 15}; int segments = 2;
+//        Integer[] radius = new Integer[] {49, 86, 69, 3, 75, 15, 62, 64, 93, 58}; int segments = 6;
+//        Integer[] radius = new Integer[] {0, 85, 1, 17, 45, 72, 90, 93, 23, 75}; int segments = 8;
+//        Integer[] radius = new Integer[] {10, 13, 94, 67, 63, 34, 83, 37, 25, 36}; int segments = 3;
+//        Integer[] radius = new Integer[] {51, 94, 55, 92, 37, 63, 62, 13, 68, 33}; int segments = 8;
+//        Integer[] radius = new Integer[] {1, 1, 1, 2, 3, 3, 3, 4, 5}; int segments = 6;
 //        Integer[] radius = new Integer[] {2, 2, 3, 3, 4, 5, 5, 6, 7, 8}; int segments = 8;
 //        Integer[] radius = new Integer[] {2, 2, 3, 3, 4, 5, 5}; int segments = 7;
 //        Integer[] radius = new Integer[] {2, 3, 3, 4, 4, 5}; int segments = 6;
@@ -179,18 +205,27 @@ public class Solution {
 //        Integer[] radius = new Integer[] {1, 1, 1, 2}; int segments = 3;
 //        Integer[] radius = new Integer[] {1, 1, 1, 2, 2}; int segments = 4;
 //        Integer[] radius = new Integer[] {3, 3, 4}; int segments = 3;
+//        Integer[] radius = new Integer[] {3, 5, 7}; int segments = 10;
+//        Integer[] radius = new Integer[] {3, 5, 7}; int segments = 8;
 //        Integer[] radius = new Integer[] {2, 7}; int segments = 16;
+//        Integer[] radius = new Integer[] {3}; int segments = 5;
 
         int numberOfCircles = 100000;
-        int segments = 100000;
+        int segments = (int)(Math.random() * 100000) + 1;
         Integer[] radius = new Integer[numberOfCircles];
-        for (int i = 0; i < numberOfCircles; i++) radius[i] = (int)(Math.random() * 10000.0);
+        for (int i = 0; i < numberOfCircles; i++) {
+            radius[i] = (int)(Math.random() * 10000.0);
+//            System.out.print(radius[i] + ", ");
+        }
+//        System.out.println();
+//        System.out.println("Segments: " + segments);
+
 
         long startTime = System.currentTimeMillis();
 
 //        String result1 = Result.largestSegmentBruteForce(Arrays.asList(radius), segments); System.out.println(result1);
-        String result1 = Result.largestSegmentBottomUpDP(Arrays.asList(radius), segments); System.out.println(result1);
 //        String result2 = Result.largestSegmentTopDownDP(Arrays.asList(radius), segments); System.out.println(result2);
+        String result3 = Result.largestSegmentStraightforward(Arrays.asList(radius), segments); System.out.println(result3);
 
         long endTime = System.currentTimeMillis();
         System.out.println("execution time: " + (endTime - startTime));
--- src/phillipscodechalenge/Solution2.java
+++ src/phillipscodechalenge/Solution2.java
@@ -1,32 +1,36 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution2 {
 
-    public static int findSolution(int[] a, int[] b, int d) {
-        Arrays.sort(a);
-        Arrays.sort(b);
+    public static int comparatorValue(List<Integer> a, List<Integer> b, int d) {
+        Collections.sort(a);
+        Collections.sort(b);
 
         int result = 0;
-        for (int i = 0; i < a.length; i++) {
-            int aIntervalStart = (a[i]-d) < 0 ? 0 : (a[i]-d);
-            int aIntervalEnd = (a[i]+d) >= a.length ? a.length-1 : (a[i]+d);
+        for (int i = 0; i < a.size(); i++) {
+            int aIntervalStart = a.get(i) - d;
+            int aIntervalEnd = a.get(i) + d;
 
             // find location of a in array b
             int bIntervalStart = 0;
-            int bIntervalEnd = b.length;
-            if (aIntervalEnd < bIntervalStart || aIntervalStart > bIntervalEnd) {
+            int bIntervalEnd = b.size() - 1;
+            if (aIntervalEnd < b.get(bIntervalStart) || aIntervalStart > b.get(bIntervalEnd)) {
                 result++;
             } else {
                 while (bIntervalEnd - bIntervalStart > 1) {
                     int middle = (bIntervalStart + bIntervalEnd) / 2;
-                    if (b[middle] < a[i]) {
+                    if (b.get(middle) < a.get(i)) {
                         bIntervalStart = middle;
                     } else {
                         bIntervalEnd = middle;
                     }
                 }
 
-                if (bIntervalStart < aIntervalStart && bIntervalEnd > aIntervalEnd) {
+                if (b.get(bIntervalStart) < aIntervalStart && b.get(bIntervalEnd) > aIntervalEnd) {
                     result++;
                 }
             }
@@ -36,11 +40,11 @@ public class Solution2 {
     }
 
     public static void main(String[] args) {
-        int[] a = {};
-        int[] b = {};
-        int d = 2;
+        Integer[] a = {3, 1, 5};
+        Integer[] b = {5, 6, 7};
+        int d = 3;
 
-        int result = findSolution(a, b, d);
+        int result = comparatorValue(Arrays.asList(a), Arrays.asList(b), d);
         System.out.println(result);
     }
 }
